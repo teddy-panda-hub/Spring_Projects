2e02a3b8881116cdccb519cb6e42ac7baf932278
teddy-panda-hub <teddypanda123456789@gmail.com>
2024-08-19 17:00:18 +0530

    Add files via upload

---
+++ ConfigServerMicroservices/src/test/java/com/test/config/ConfigServerMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
